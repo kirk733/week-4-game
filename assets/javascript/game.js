db598b5c45d680a5689c66e5db4127ffdc4c6b61
kirk <kirk733@MacBook.local>
2017-05-30 19:36:30 -0600

    something wrong with resetVariables function

---
+++ assets/javascript/game.js
@@ -0,0 +1,76 @@
+
+//make sure document ready before page loads
+$(document).ready(function() {
+
+ 	//variable declaration
+	var wins = 0;
+	var losses = 0;
+	var totalScore = 0;
+	var randomNumber = Math.floor(Math.random() * 101) + 19;
+	var blue = Math.floor(Math.random() * 12) + 1;
+	var red = Math.floor(Math.random() * 11) + 2;
+	var diamond = Math.floor(Math.random() * 10) + 3;
+	var green = Math.floor(Math.random() * 9) + 4;
+
+	console.log(blue, red, diamond, green);
+
+
+	$("#wins").html("" + wins);
+	$("#losses").html("" + losses);
+	$("#randomNumber").html("" + randomNumber);
+	console.log(randomNumber);
+
+	$("#button1").on("click", function() {
+  		totalScore = totalScore + blue;
+ 		 $("#totalScore").html("" + totalScore);
+ 		 winLoss ();
+	});
+
+	$("#button2").on("click", function() {
+  		totalScore = totalScore + red;
+ 		 $("#totalScore").html("" + totalScore);
+ 		 winLoss ();
+	});
+
+	$("#button3").on("click", function() {
+  		totalScore = totalScore + diamond;
+ 		 $("#totalScore").html("" + totalScore);
+ 		 winLoss ();
+	});
+
+	$("#button4").on("click", function() {
+  		totalScore = totalScore + green;
+ 		 $("#totalScore").html("" + totalScore);
+ 		 winLoss ();
+	});
+
+	function winLoss(){
+		if (randomNumber === totalScore) {
+    	wins++
+    	$("#wins").html("" + wins);
+    	resetVariables();
+    	};
+    
+
+		if (totalScore > randomNumber) {
+    	losses++	
+    	$("#losses").html("" + losses);
+    	resetVariables ();
+  		};
+  	};
+  		
+  	function resetVariables(){
+  		randomNumber = Math.floor(Math.random() * 101) + 19;
+		blue = Math.floor(Math.random() * 12) + 1;
+		red = Math.floor(Math.random() * 11) + 2;
+		diamond = Math.floor(Math.random() * 10) + 3;
+		green = Math.floor(Math.random() * 9) + 4;
+		totalScore = 0;
+  		};
+
+
+	
+	
+
+
+});
